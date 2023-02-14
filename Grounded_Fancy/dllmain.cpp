33b9fc32615c6a1ff6c524fc38bf19a416016959
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-02-14 07:40:40 -0800

    Base Commit

---
+++ Grounded_Fancy/dllmain.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    if(ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+    }
+    else if(ul_reason_for_call == DLL_PROCESS_DETACH)
+    {
+        
+    }
+    return TRUE;
+}
+
